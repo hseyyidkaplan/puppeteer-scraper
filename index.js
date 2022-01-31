e67e2ef3587e7ecccd5f2ecbb14de059da4ce6db
hseyyidkaplan <hseyyidkaplan@gmail.com>
2022-01-31 23:26:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+
+function dateCalculator (postDateString) {
+    // postDateString in format yyyy-mm-dd
+    let currentDate = new Date();
+    let postDate = new Date(postDateString);
+    let diffTime = currentDate.getTime() - postDate.getTime();
+    let diffDays= diffTime / (1000 * 3600 * 24);
+    
+    if (diffDays > 365) return false;
+    return true;
+}
+
+async function getTitles() {
+    const browser = await puppeteer.launch({});
+
+    const page = await browser.newPage();
+
+    const url = 'https://filmloverss.com/kategori/haberler/';
+
+    await page.goto(url);
+
+    await page.waitForSelector('#cb-content > div > article');
+
+    const titles = await page.$$eval('#cb-content > div > article', posts => {
+        return posts.map(post => {
+            const time = post.querySelector('div.cb-meta > div.cb-byline > span.cb-date > time');
+            const properties = {};
+            if (dateCalculator(time)) {
+                const titleElement = post.querySelector('div.cb-meta > h2 > a');
+                properties.title = titleElement.innerText;
+                properties.url = titleElement.getAttribute('href');
+            }
+            return properties;
+        });
+    });
+
+    console.log(titles);
+
+    browser.close();
+}
+
+getTitles();
+
+//#post-140936 > div.cb-meta > div.cb-byline > span.cb-date > time
+
+//#cb-content > div > article // her biri bir post oluyor
